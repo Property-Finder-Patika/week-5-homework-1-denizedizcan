9a90fe011614df238f9a01ab590e5d87a5bbc83e
denizedizcan <70265563+denizedizcan@users.noreply.github.com>
2022-07-31 19:31:58 +0300

    race condition

---
+++ race.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	follower int
+}
+
+func (a *Person) getFollower() {
+	a.follower += 1
+}
+
+func (a *Person) loseFollower() {
+	if 0 > a.follower {
+		fmt.Printf("already 0 follower: %d\n", a.follower)
+	} else {
+		time.Sleep(10 * time.Millisecond)
+		a.follower -= 1
+	}
+}
+
+func main() {
+	var Person *Person = &Person{20}
+	fmt.Printf("follower at the beginning: %d\n", Person.follower)
+	for i := 1; i <= 10000; i++ {
+		go losingFollower(i, Person)
+		go gainFollower(i, Person)
+	}
+	time.Sleep(1 * time.Second)
+	fmt.Printf("follower at the end: %d\n", Person.follower)
+}
+
+func losingFollower(count int, a *Person) {
+	fmt.Printf("%d: follower before: %d\n", count, a.follower)
+	a.loseFollower()
+	fmt.Printf("%d: follower after: %d\n", count, a.follower)
+}
+
+func gainFollower(count int, a *Person) {
+	a.getFollower()
+	fmt.Printf("%d: follower after: %d\n", count, a.follower)
+}
