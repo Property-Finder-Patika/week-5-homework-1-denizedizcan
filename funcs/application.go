cfde30662c98b54fa40c1e2e94b40fd3104ea42b
denizedizcan <70265563+denizedizcan@users.noreply.github.com>
2022-07-31 19:12:34 +0300

    proxy pattern comments

--- funcs/application.go
+++ funcs/application.go
@@ -3,10 +3,12 @@ package funcs
 type Application struct {
 }
 
-func (a *Application) handleRequest(url, method string) (int, string) {
+//Aplication requests control
+func (a *Application) HandleRequest(url, method string) (int, string) {
 	if url == "/licence" && method == "GET" {
 		return 200, "Ok"
+	} else if url == "/licence/loguot" && method == "POST" {
+		return 201, "Succesful Logout"
 	}
-
 	return 404, "Not Ok"
 }
--- funcs/proxy.go
+++ funcs/proxy.go
@@ -1,32 +1,64 @@
 package funcs
 
+import "sync"
+
+var (
+	mu sync.Mutex
+)
+
+//proxy
 type Nginx struct {
-	application       *Application
-	maxAllowedRequest int
-	licenceCount      int
+	application     *Application
+	maxLicenceCount int
+	licenceCount    int
 }
 
-func newNginxServer() *Nginx {
+//create proxy
+func NewNginxServer() *Nginx {
 	return &Nginx{
-		application:       &Application{},
-		maxAllowedRequest: 10,
-		licenceCount:      0,
+		application:     &Application{},
+		maxLicenceCount: 10,
+		licenceCount:    0,
 	}
 }
 
-func (n *Nginx) handleRequest(url, method string) (int, string) {
-	allowed := n.checkLicenceCount(url)
-	if !allowed {
-		return 403, "Not Allowed"
+//proxy handle request
+func (n *Nginx) HandleRequest(url, method string) (int, string) {
+	value, response := n.application.HandleRequest(url, method)
+
+	if value == 200 {
+		// licence count < max licence count
+		allowed := n.CheckLicenceCount(url)
+
+		if !allowed {
+			//licence count >= mac licence count
+			return 403, "Not Allowed"
+		}
+	} else if value == 201 {
+		// logut licence
+		n.LogutLicenceCount(url)
 	}
-	return n.application.handleRequest(url, method)
+	return value, response
 }
 
-func (n *Nginx) checkLicenceCount(url string) bool {
-
-	if n.licenceCount > n.maxAllowedRequest {
+func (n *Nginx) CheckLicenceCount(url string) bool {
+	// get once licence
+	mu.Lock()
+	defer mu.Unlock()
+	if n.licenceCount >= n.maxLicenceCount {
 		return false
 	}
 	n.licenceCount += 1
+	return true
+}
+
+func (n *Nginx) LogutLicenceCount(url string) bool {
+	// gave back licence
+	mu.Lock()
+	defer mu.Unlock()
+	if n.licenceCount <= 0 {
+		return false
+	}
+	n.licenceCount -= 1
 	return true
 }
